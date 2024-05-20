25f6e957c4f0da50fae0e75e19f5597803fbf530
Antoine Landrieux <101741720+AntoineLandrieux@users.noreply.github.com>
2024-05-20 15:47:44 +0200

    v1.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+
+# CodeStud changelog
+
+---
+
+v1.1.0
+
+- Add JS console
+- Fix couldn't do tab
--- javascript/script.js
+++ javascript/script.js
@@ -13,7 +13,7 @@ Open-Source online text editor
 ***********************************************
 */
 
-const CODESTUD_VERSION = {MAJOR: 1, MINOR: 0, PATCH: 0};
+const CODESTUD_VERSION = { MAJOR: 1, MINOR: 1, PATCH: 0 };
 
 let files = [];
 let current_file = "";
@@ -46,15 +46,15 @@ function file_save(filename) {
 function file_saveas(filename) {
 
     files.forEach((item) => {
-        
+
         if (item.name.toLowerCase() == filename.toLowerCase()) {
-            var textBlob = new Blob([item.content], {type: "text/plain"});
+            var textBlob = new Blob([item.content], { type: "text/plain" });
             var download = document.createElement("a");
-            
+
             download.download = filename;
             download.href = window.URL.createObjectURL(textBlob);
             download.click();
-            
+
             delete textBlob;
             delete download;
         }
@@ -120,19 +120,39 @@ document.getElementById("files").addEventListener("dblclick", function () {
     file_create(prompt("New file"), "");
 });
 
+code.addEventListener("keydown", (event) => {
+    if (event.key != "Tab")
+        return;
+    event.preventDefault();
+    code.setRangeText('\t', code.selectionStart, code.selectionStart, "end");
+    return false;
+})
+
 code.addEventListener("keyup", () => {
     file_save(current_file);
 });
 
-document.getElementById("saveas").addEventListener('click', function() {
+document.getElementById("saveas").addEventListener('click', function () {
     file_saveas(current_file);
+});
+
+document.getElementById("cmd").addEventListener('click', function () {
+    document.getElementById("console").classList.toggle("hide");
+});
+
+document.getElementById("run").addEventListener('click', function () {
+    document.getElementById("out").innerText += document.getElementById("input").value + "\n";
+    try {
+        document.getElementById("out").innerText += eval(document.getElementById("input").value) + "\n\n[JS]>";
+    } catch (except) {
+        document.getElementById("out").innerText += except + "\n\n[JS]>";
+    }
 });
 
 window.onbeforeunload = () => { return "You have attempted to leave this page. Are you sure?"; };
 
 window.addEventListener('load', function () {
     file_create("Hello.txt", `
-
 ***********************************************
  _____       _     _____ _         _ 
 |     |___ _| |___|   __| |_ _ _ _| |
@@ -148,7 +168,7 @@ Open-Source online text editor
 
 -> Double click on the topbar to create a new file
 -> Click the button at the bottom left to save the current file
-`
-    );
+`);
     file_load("Hello.txt");
+    document.getElementById("out").innerHTML = `[JS]>`;
 });
